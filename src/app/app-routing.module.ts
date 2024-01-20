65ec755cb6a403d7daad533f6e6518519e8da4c8
joyce-coder-lumia <tchavessrose@gmail.com>
2024-01-20 23:50:39 +0000

    avancement front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SignComponent } from './pages/sign/sign.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './sharepages/navbar/navbar.component';
+import { TransretraitComponent } from './pages/transretrait/transretrait.component';
+import { TransversComponent } from './pages/transvers/transvers.component';
+import { TransvirementComponent } from './pages/transvirement/transvirement.component';
+import { ComptelistComponent } from './pages/comptelist/comptelist.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'sign',component:SignComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'transretrait',component:TransretraitComponent},
+  {path:'transvers',component:TransversComponent},
+  {path:'transvirement',component:TransvirementComponent},
+  {path:'comptelist',component:ComptelistComponent}
    
 ];
 
